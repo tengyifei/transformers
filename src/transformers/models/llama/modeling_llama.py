2f9527430b45f92fe99b94cfe941ddac62c1d24b
Jiewen Tan <jwtan@google.com>
2024-04-19 23:19:40 +0000

    Enable causal mask

--- src/transformers/models/llama/modeling_llama.py
+++ src/transformers/models/llama/modeling_llama.py
@@ -381,7 +381,7 @@ class LlamaAttention(nn.Module):
 
         # Integrated with PyTorch/XLA Pallas Flash Attention:
         from torch_xla.experimental.custom_kernel import flash_attention
-        attn_output = flash_attention(query_states, key_states, value_states, partition_spec=('fsdp', None, None, None))
+        attn_output = flash_attention(query_states, key_states, value_states, causal=True, partition_spec=('fsdp', None, None, None))
 
         if attn_output.size() != (bsz, self.num_heads, q_len, self.head_dim):
             raise ValueError(
