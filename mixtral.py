8b920c5c6d3850197bf5aa6d6a6cbbf7d26d8bc2
Jiewen Tan <jwtan@google.com>
2024-05-18 01:16:48 +0000

    Make the script works with tpu again

--- mixtral.py
+++ mixtral.py
@@ -19,13 +19,12 @@ config = AutoConfig.from_pretrained(
 )
 print(config)
 
-# device = xm.xla_device()
 device = 'cpu'
 
-config.static=False
-torch.manual_seed(42)
-dynamic_model = MixtralForCausalLM(config).to(device)
-print(f"Model parameters: {dynamic_model.num_parameters()/2**20:.2f}M params")
+# config.static=False
+# torch.manual_seed(42)
+# dynamic_model = MixtralForCausalLM(config).to(device)
+# print(f"Model parameters: {dynamic_model.num_parameters()/2**20:.2f}M params")
 
 # This is a custom config to enable the static mode of expert computation.
 config.static=True
@@ -33,14 +32,17 @@ torch.manual_seed(42)
 static_model = MixtralForCausalLM(config).to(device)
 print(f"Model parameters: {static_model.num_parameters()/2**20:.2f}M params")
 
-input_sizes = [8, 128, 256, 512, 1024]
-for input_size in input_sizes:
-    input = torch.randint(128, ((2, input_size // 2))).to(device)
-    static_output = static_model(input)
-    # print(static_output.logits.shape)
-    dynamic_output = dynamic_model(input)
-    # print(dynamic_output.logits.shape)
-    assert torch.allclose(static_output.logits, dynamic_output.logits, atol=1e-6), "logits are not equal"
+# input_sizes = [8, 128, 256, 512, 1024]
+# for input_size in input_sizes:
+#     input = torch.randint(128, ((2, input_size // 2))).to(device)
+#     static_output = static_model(input)
+#     print(static_output.logits.shape)
+#     dynamic_output = dynamic_model(input)
+#     print(dynamic_output.logits.shape)
+#     assert torch.allclose(static_output.logits, dynamic_output.logits, atol=1e-6), "logits are not equal"
 
-# xm.mark_step()
-# print(met.metrics_report())
+device = xm.xla_device()
+model = static_model.to(device)
+output = model(torch.randint(128, ((2, 128))).to(device))
+xm.mark_step()
+print(met.metrics_report())
