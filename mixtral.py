606edad82df4965677b31f2dfcd2b955a03c3df4
Jiewen Tan <jwtan@google.com>
2024-05-18 01:12:57 +0000

    test correctness

--- mixtral.py
+++ mixtral.py
@@ -21,15 +21,26 @@ print(config)
 
 # device = xm.xla_device()
 device = 'cpu'
+
+config.static=False
 torch.manual_seed(42)
+dynamic_model = MixtralForCausalLM(config).to(device)
+print(f"Model parameters: {dynamic_model.num_parameters()/2**20:.2f}M params")
 
 # This is a custom config to enable the static mode of expert computation.
 config.static=True
-model = MixtralForCausalLM(config).to(device)
-print(f"Model parameters: {model.num_parameters()/2**20:.2f}M params")
+torch.manual_seed(42)
+static_model = MixtralForCausalLM(config).to(device)
+print(f"Model parameters: {static_model.num_parameters()/2**20:.2f}M params")
 
-output = model(torch.arange(8).view(1, 8).to(device))
+input_sizes = [8, 128, 256, 512, 1024]
+for input_size in input_sizes:
+    input = torch.randint(128, ((2, input_size // 2))).to(device)
+    static_output = static_model(input)
+    # print(static_output.logits.shape)
+    dynamic_output = dynamic_model(input)
+    # print(dynamic_output.logits.shape)
+    assert torch.allclose(static_output.logits, dynamic_output.logits, atol=1e-6), "logits are not equal"
+
 # xm.mark_step()
-print(output.logits.shape)
-print(output.logits)
 # print(met.metrics_report())
